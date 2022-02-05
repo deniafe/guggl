25b51a5baff626127eff117f238ec21d3056039b
deniafe <detoyin@yahoo.com>
2022-02-05 20:31:43 +0100

    first commit

---
+++ src/components/Routes.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import { Results } from './Results'
+
+
+export const Pages = () => {
+  return (
+    <div className="p-4">
+      <Routes>
+        <Route path="/" element={<Results />} />
+        <Route path="/search" element={<Results />} />
+        <Route path="/images" element={<Results />} />
+        <Route path="/news" element={<Results />} />
+        <Route path="/videos" element={<Results />} />
+      </Routes>
+
+    </div>
+  )
+}
